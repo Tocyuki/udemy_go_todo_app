45cea46176f8dc9893d2724c8f08611bab5c1f2a
Tocyuki <tocguitar1@gmail.com>
2024-12-24 23:15:19 +0900

    2-3 User.go GetUser

--- todo_app/main.go
+++ todo_app/main.go
@@ -9,11 +9,14 @@ import (
 func main() {
 	fmt.Println(models.Db)
 
-	u := &models.User{}
-	u.Name = "test"
-	u.Email = "test@example.com"
-	u.PassWord = "testtest"
-	fmt.Println(u)
+	// u := &models.User{}
+	// u.Name = "test"
+	// u.Email = "test@example.com"
+	// u.PassWord = "testtest"
+	// fmt.Println(u)
+	//
+	// u.CreateUser()
 
-	u.CreateUser()
+	u, _ := models.GetUser(1)
+	fmt.Println(u)
 }
