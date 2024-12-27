03f697d90e16d914417f2b1678a94f26460042ff
Tocyuki <tocguitar1@gmail.com>
2024-12-28 00:24:30 +0900

    2-6 Todo.go DB作成

--- todo_app/main.go
+++ todo_app/main.go
@@ -17,16 +17,16 @@ func main() {
 	//
 	// u.CreateUser()
 
-	u, _ := models.GetUser(1)
-	fmt.Println(u)
-
-	u.Name = "Test2"
-	u.Email = "test2@example.com"
-	u.UpdateUser()
-	u, _ = models.GetUser(1)
-	fmt.Println(u)
-
-	u.DeleteUser()
-	u, _ = models.GetUser(1)
-	fmt.Println(u)
+	// u, _ := models.GetUser(1)
+	// fmt.Println(u)
+	//
+	// u.Name = "Test2"
+	// u.Email = "test2@example.com"
+	// u.UpdateUser()
+	// u, _ = models.GetUser(1)
+	// fmt.Println(u)
+	//
+	// u.DeleteUser()
+	// u, _ = models.GetUser(1)
+	// fmt.Println(u)
 }
