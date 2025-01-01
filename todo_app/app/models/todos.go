286eacd9d051845cb13765e310d33cf30aa6ecdb
Tocyuki <tocguitar1@gmail.com>
2025-01-01 23:14:06 +0900

    3-4 Todo.go GetTodos

--- todo_app/app/models/todos.go
+++ todo_app/app/models/todos.go
@@ -29,3 +29,23 @@ func GetTodo(id int) (todo Todo, err error) {
 
 	return todo, err
 }
+
+func GetTodos() (todos []Todo, err error) {
+	cmd := `select id, content, user_id, created_at from todos`
+	rows, err := Db.Query(cmd)
+	defer rows.Close()
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	for rows.Next() {
+		var todo Todo
+		err = rows.Scan(&todo.ID, &todo.Content, &todo.UserID, &todo.CreatedAt)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		todos = append(todos, todo)
+	}
+	return todos, err
+}
